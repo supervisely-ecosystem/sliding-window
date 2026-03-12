ad611117a49f9781aabbdf49689c3ae65ad69341
vorozhkog <vorozhkog@icloud.com>
2026-03-12 16:40:02 +0000

    init

--- merge/src/main.py
+++ merge/src/main.py
@@ -3,6 +3,16 @@ from collections import defaultdict
 
 import globals as g
 import numpy as np
+if not hasattr(np, "sctypes"):
+    np.sctypes = {
+        "int": [np.int8, np.int16, np.int32, np.int64],
+        "uint": [np.uint8, np.uint16, np.uint32, np.uint64],
+        "float": [np.float16, np.float32, np.float64, np.longdouble],
+        "complex": [np.complex64, np.complex128, np.clongdouble],
+        "others": [np.bool_, np.object_, np.str_, np.bytes_, np.void],
+        "datetime": [np.datetime64],
+        "timedelta": [np.timedelta64],
+    }
 import supervisely as sly
 from supervisely.geometry.sliding_windows_fuzzy import SlidingWindowBorderStrategy
 from typing import List
